01dc1419470bb1a69673ca8f67db4154c2466ae2
prashant0664 <prashantthakran0664@gmail.com>
2024-03-31 21:08:06 +0530

    added

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    webpack: (config) => {
+        config.resolve.fallback = {
+          "mongodb-client-encryption": false ,
+          "aws4": false
+        }
+    }    
 };
 
 export default nextConfig;
